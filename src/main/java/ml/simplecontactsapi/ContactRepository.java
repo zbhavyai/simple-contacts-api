f53fed194f71d8ca30bc489b2820c278a7086b4f
Bhavyai Gupta <zbhavyai@gmail.com>
2022-04-10 17:20:34 -0600

    removing unused method

--- src/main/java/ml/simplecontactsapi/ContactRepository.java
+++ src/main/java/ml/simplecontactsapi/ContactRepository.java
@@ -1,14 +1,9 @@
 package ml.simplecontactsapi;
 
-import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import io.quarkus.hibernate.reactive.panache.PanacheRepository;
-import io.smallrye.mutiny.Uni;
 
 @ApplicationScoped
 public class ContactRepository implements PanacheRepository<Contact> {
 
-    public Uni<Contact> findByName(String name) {
-        return find("name", name).firstResult();
-    }
 }
