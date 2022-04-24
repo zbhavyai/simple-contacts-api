188bad8cda92ee37ecbfc2d5b150b99915184936
Bhavyai Gupta <zbhavyai@gmail.com>
2022-04-24 15:41:17 -0600

    adding parameter in graphql client get request

--- graphql-client/src/main/java/ml/simplecontactsclient/ContactClient.java
+++ graphql-client/src/main/java/ml/simplecontactsclient/ContactClient.java
@@ -4,14 +4,16 @@ import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.jboss.resteasy.reactive.RestPath;
 import io.smallrye.common.annotation.Blocking;
 import io.smallrye.graphql.client.GraphQLClient;
 import io.smallrye.graphql.client.Response;
 import io.smallrye.graphql.client.core.Document;
 import io.smallrye.graphql.client.dynamic.api.DynamicGraphQLClient;
 import io.smallrye.mutiny.Uni;
-// import javax.json.JsonObject;
 import static io.smallrye.graphql.client.core.Document.document;
+import static io.smallrye.graphql.client.core.Argument.arg;
+import static io.smallrye.graphql.client.core.Argument.args;
 import static io.smallrye.graphql.client.core.Field.field;
 import static io.smallrye.graphql.client.core.Operation.operation;
 
@@ -36,6 +38,23 @@ public class ContactClient {
         // Response response = dynamicClient.executeSync(query);
         // return response.getData();
 
-        return dynamicClient.executeAsync(query);
+        return this.dynamicClient.executeAsync(query);
+    }
+
+    @GET
+    @Path("/dynamic/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Blocking
+    public Uni<Response> getAllContactsByNameUsingDynamicClient(@RestPath String name) throws Exception {
+        Document query = document(
+                operation(
+                        field("getContactByName", args(arg("name", name)),
+                                field("firstName"),
+                                field("nickname"),
+                                field("phone"),
+                                field("email"),
+                                field("lastName"))));
+
+        return this.dynamicClient.executeAsync(query);
     }
 }
